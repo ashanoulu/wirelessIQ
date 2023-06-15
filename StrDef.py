ab4983a974991a83c1ee5da107013b8ea7548718
Janani Kehelwala <kkehelwa23@univ.yo.oulu.fi>
2023-06-15 16:31:32 +0300

    Updated

--- StrDef.py
+++ StrDef.py
@@ -10,9 +10,12 @@ class StrDef:
     SEN_LIGHT_HIGH2 = "light_h2"
 
     ST_TEMPERATURE_AVG = "temp_avg"
+    ST_TEMPERATURE_SDV = "temp_std"
     ST_TEMPERATURE_MAX = "temp_max"
     ST_AIR_QUALITY_AVG = "aq_avg"
+    ST_AIR_QUALITY_SDV = "aq_std"
     ST_AIR_QUALITY_WORST = "aq_worst"  # max co2
     ST_LIGHT_AVG = "light_avg"
+    ST_LIGHT_SDV = "light_std"
     ST_ARR_WINDOW_STATUS = "window_status"
     ST_ARTIFICIAL_LIGHT_STATUS = "art_light_status"
--- publisher_new.py
+++ publisher_new.py
@@ -6,8 +6,8 @@ import util
 import sensor_api as sapi
 import smbus
 
-from wirelessIQ.StrDef import StrDef
-from wirelessIQ.timer_wp import timer
+from StrDef import StrDef
+from timer_wp import timer
 import numpy as np
 
 # WirelessIQ Parameters
@@ -33,8 +33,8 @@ sgp30_sensor = sapi.sensor_sgp30()  # Air quality sensor
 
 array_sensor_keys = [
     StrDef.SEN_TEMPERATURE
-    , StrDef.SEN_HUMIDITY
-    , StrDef.SEN_PRESSURE
+    # , StrDef.SEN_HUMIDITY
+    # , StrDef.SEN_PRESSURE
     , StrDef.SEN_AIR_QUALITY
     , StrDef.SEN_LIGHT_LOW
     , StrDef.SEN_LIGHT_HIGH1
@@ -43,10 +43,13 @@ array_sensor_keys = [
 
 array_stat_keys = [
     StrDef.ST_TEMPERATURE_AVG
+    , StrDef.ST_TEMPERATURE_SDV
     , StrDef.ST_TEMPERATURE_MAX
     , StrDef.ST_AIR_QUALITY_AVG
+    , StrDef.ST_AIR_QUALITY_SDV
     , StrDef.ST_AIR_QUALITY_WORST
     , StrDef.ST_LIGHT_AVG
+    , StrDef.ST_LIGHT_SDV
     , StrDef.ST_ARR_WINDOW_STATUS
     , StrDef.ST_ARTIFICIAL_LIGHT_STATUS
 ]
@@ -73,11 +76,11 @@ class SensorDataCollection:
         self.data_temp = np_array_init_and_fill(self.arr_size, 0)
         self.timestamps_temp = np_array_init_and_fill(self.arr_size, 0)
 
-        self.data_pressure = np_array_init_and_fill(self.arr_size, 0)
-        self.timestamps_pressure = np_array_init_and_fill(self.arr_size, 0)
-
-        self.data_humidity = np_array_init_and_fill(self.arr_size, 0)
-        self.timestamps_humidity = np_array_init_and_fill(self.arr_size, 0)
+        # self.data_pressure = np_array_init_and_fill(self.arr_size, 0)
+        # self.timestamps_pressure = np_array_init_and_fill(self.arr_size, 0)
+        #
+        # self.data_humidity = np_array_init_and_fill(self.arr_size, 0)
+        # self.timestamps_humidity = np_array_init_and_fill(self.arr_size, 0)
 
         # Air quality - SGP30
         self.data_airquality = np_array_init_and_fill(self.arr_size, 0)
@@ -107,21 +110,21 @@ class SensorDataCollection:
         self.timestamps_temp[self.counter] = ts_temp
         return 0
 
-    @timer
-    def collect_humidity(self):
-        # Humidity sample
-        humidity_sample, ts_humid = bme680_sensor.get_humidity()
-        self.data_humidity[self.counter] = humidity_sample
-        self.timestamps_humidity[self.counter] = ts_humid
-        return 0
-
-    @timer
-    def collect_pressure(self):
-        # Pressure sample
-        pressure_sample, ts_pressure = bme680_sensor.get_pressure()
-        self.data_pressure[self.counter] = pressure_sample
-        self.timestamps_pressure[self.counter] = ts_pressure
-        return 0
+    # @timer
+    # def collect_humidity(self):
+    #     # Humidity sample
+    #     humidity_sample, ts_humid = bme680_sensor.get_humidity()
+    #     self.data_humidity[self.counter] = humidity_sample
+    #     self.timestamps_humidity[self.counter] = ts_humid
+    #     return 0
+    #
+    # @timer
+    # def collect_pressure(self):
+    #     # Pressure sample
+    #     pressure_sample, ts_pressure = bme680_sensor.get_pressure()
+    #     self.data_pressure[self.counter] = pressure_sample
+    #     self.timestamps_pressure[self.counter] = ts_pressure
+    #     return 0
 
     @timer
     def collect_air_quality(self):
@@ -173,14 +176,14 @@ class SensorDataCollection:
         time.sleep(CONST_SLEEP_TIMER)  # TODO when to sleep
         et_start = sapi.get_timestamp()
         r_temp, et_temp = self.collect_temperature()
-        r_humidity, et_humidity = self.collect_humidity()
-        r_pressure, et_pressure = self.collect_pressure()
+        # r_humidity, et_humidity = self.collect_humidity()
+        # r_pressure, et_pressure = self.collect_pressure()
         r_air_quality, et_air_quality = self.collect_air_quality()
         r_light, et_light = self.collect_light()
-        r_window_status, et_window_status  = self.collect_window_status()
+        r_window_status, et_window_status = self.collect_window_status()
         r_light_status, et_light_status = self.collect_light_status()
-        print("et_start, et_temp, et_humidity, et_pressure, et_air_quality, et_light, et_window_status, et_light_status, et_end")
-        print(et_start, et_temp, et_humidity, et_pressure, et_air_quality, et_light, et_window_status, et_light_status, sapi.get_timestamp())
+        print("et_start, et_temp, et_air_quality, et_light, et_window_status, et_light_status, et_end")
+        print(et_start, et_temp, et_air_quality, et_light, et_window_status, et_light_status, sapi.get_timestamp())
 
         self.counter += 1
 
@@ -207,8 +210,8 @@ class SensorDataCollection:
 
             array_timestamps = [
                 [self.timestamps_temp[0]]
-                , [self.timestamps_humidity[0]]
-                , [self.timestamps_pressure[0]]
+                # , [self.timestamps_humidity[0]]
+                # , [self.timestamps_pressure[0]]
                 , [self.timestamps_airquality[0]]
                 , [self.timestamps_lowres[0]]
                 , [self.timestamps_highres[0]]
@@ -218,8 +221,8 @@ class SensorDataCollection:
             # TODO numpy to list conversion?
             array_sensor_values = [
                 self.data_temp
-                , self.data_humidity
-                , self.data_pressure
+                # , self.data_humidity
+                # , self.data_pressure
                 , self.data_airquality
                 , self.data_lowres
                 , self.data_highres
@@ -239,20 +242,26 @@ class SensorDataCollection:
 
             array_stat_values = [
                 [self.data_temp.mean()]
+                , [self.data_temp.std()]
                 , [self.data_temp[data_temp_max_index]]
                 , [self.data_airquality.mean()]
+                , [self.data_airquality.std()]
                 , [self.data_airquality[data_co2_max_index]]
                 , [self.data_highres.mean()]  # TODO which light measure to use?
+                , [self.data_highres.std()]
                 , self.stat_window_status
                 , self.stat_light_status
             ]
 
             array_stat_ts = [
                 [self.timestamps_temp[0]]
+                , [self.timestamps_temp[0]]
                 , [self.timestamps_temp[data_temp_max_index]]
+                , [self.timestamps_airquality[0]]
                 , [self.timestamps_airquality[0]]
                 , [self.timestamps_airquality[data_co2_max_index]]
                 , [self.timestamps_highres[0]]  # TODO which light measure to use?
+                , [self.timestamps_highres[0]]
                 , self.stat_window_status_ts
                 , self.stat_light_status_ts
             ]
@@ -276,5 +285,3 @@ if __name__ == '__main__':
 # TODO search and remove print() commands values
 # TODO Remove unnecessary sensor collection
 # Opt : send min:second and infer hour from received time : sensor_api.py: get_timestamp():
-# Q: Lux measures illuminance, luminosity same thing?
-# Q: Light on off?  Natural light vs artificial light
